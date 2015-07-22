6ded468cd7cb238f3a9df3cdb46d95811bfafa74
ofreax <bertrand.gouny@gmail.com>
2015-07-22 23:04:31 +0200

    delete RUN

--- image/service/slapd/install.sh
+++ image/service/slapd/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # Add openldap user and group first to make sure their IDs get assigned consistently, regardless of whatever dependencies get added
-RUN groupadd -r openldap && useradd -r -g openldap openldap
+groupadd -r openldap && useradd -r -g openldap openldap
 
 # Install OpenLDAP, ldap-utils
 LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes --no-install-recommends \
