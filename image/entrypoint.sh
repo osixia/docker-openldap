078f784873bc1dec340b7aedcb4ff2ac0d35c9be
LJ Johnson <lj020326@gmail.com>
2019-07-15 16:02:31 -0400

    updates

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -5,8 +5,21 @@
 PUID=${PUID:-911}
 PGID=${PGID:-911}
 
-groupmod -o -g "$PGID" openldap
-usermod -o -u "$PUID" openldap
+## ref: https://github.com/sudo-bmitch/jenkins-docker/blob/master/entrypoint.sh
+# get current group of openldap user inside container
+#CUR_USER_GID=`getent group openldap | cut -f3 -d: || true`
+#CUR_USER_UID=`id openldap | cut -f1 -d' ' | cut -f2 -d= | cut -f1 -d"(" || true`
+
+CUR_USER_GID=`id -g openldap || true`
+CUR_USER_UID=`id -u openldap || true`
+
+# if they don't match, adjust
+if [ ! -z "$PUID" -a "$PUID" != "$CUR_USER_UID" ]; then
+    usermod -o -u "$PUID" openldap
+fi
+if [ ! -z "$PGID" -a "$PGID" != "$CUR_USER_GID" ]; then
+    groupmod -o -g "$PGID" openldap
+fi
 
 echo '
 -------------------------------------
@@ -17,16 +30,8 @@ User uid:    $(id -u openldap)
 User gid:    $(id -g openldap)
 -------------------------------------
 "
-#chown openldap:openldap /app
-#chown openldap:openldap /config
-#chown openldap:openldap /defaults
-
-## fix file permissions
-#chown -R openldap:openldap /var/lib/ldap
-#chown -R openldap:openldap /etc/ldap
-#chown -R openldap:openldap ${CONTAINER_SERVICE_DIR}/slapd
 
 #exec /init
-#exec /container/tool/run "$@"
-echo "\$@=${@}"
-/container/tool/run "$@"
+#echo "\$@=${@}"
+#/container/tool/run "$@"
+exec /container/tool/run "$@"
