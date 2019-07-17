32a7b924c943cf2e84c76f1efa1cac804f66c420
LJ Johnson <lj020326@gmail.com>
2019-07-17 15:39:52 -0400

    updates

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -e
 
-PUID=${PUID:-911}
-PGID=${PGID:-911}
+LDAP_OPENLDAP_UID=${LDAP_OPENLDAP_UID:-911}
+LDAP_OPENLDAP_GID=${LDAP_OPENLDAP_GID:-911}
 
 # get current group of openldap user inside container
 CUR_USER_GID=`id -g openldap || true`
 CUR_USER_UID=`id -u openldap || true`
 
 # if they don't match, adjust
-if [ ! -z "$PUID" -a "$PUID" != "$CUR_USER_UID" ]; then
-    usermod -o -u "$PUID" openldap
+if [ "$LDAP_OPENLDAP_UID" != "$CUR_USER_UID" ]; then
+    usermod -o -u "$LDAP_OPENLDAP_UID" openldap
 fi
-if [ ! -z "$PGID" -a "$PGID" != "$CUR_USER_GID" ]; then
-    groupmod -o -g "$PGID" openldap
+if [ "$LDAP_OPENLDAP_GID" != "$CUR_USER_GID" ]; then
+    groupmod -o -g "$LDAP_OPENLDAP_GID" openldap
 fi
 
 echo '
